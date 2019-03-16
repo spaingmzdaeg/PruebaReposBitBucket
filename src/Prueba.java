9067128db7e55c048ca63f1c580e9454c39d2506
Daniel Espana <vivalarazad@gmail.com>
2019-03-16 14:26:00 -0600

    segundo commit

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,5 +1,8 @@
 public class Prueba {
     public static void main(String[] args) {
         System.out.println(7);
+        System.out.println(19);
+        System.out.println(23);
+        System.out.println(12);
     }
 }
